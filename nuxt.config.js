065696635693df5a46f75f8255cb82496266e665
Adam Alfredsson <alfredsson.adam@gmail.com>
2020-09-01 19:44:21 +0200

    update description

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'Välkommen till Restaurangen på Stora Holm!',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
