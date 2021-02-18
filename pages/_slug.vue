51b0541572eb95062f5840bb16a4abe4a0ba00ec
Adam Alfredsson <alfredsson.adam@gmail.com>
2021-02-18 23:35:25 +0100

    Add slug page

---
+++ pages/_slug.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+nuxt-content(:document='page')
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $content, params }) {
+    const page = await $content(`articles/${params.slug}`).fetch()
+
+    return {
+      page,
+    }
+  },
+})
+</script>
